889a9ac1ab0c6fad3147881fe2553310f9e8f07e
tgh9000 <144217697+tgh9000@users.noreply.github.com>
2025-10-18 20:46:44 +0100

    finishing the tutorial

--- Scripts/text_box.gd
+++ Scripts/text_box.gd
@@ -1,9 +1,13 @@
 extends CanvasLayer
-@onready var text = get_node("MarginContainer/Panel/MarginContainer/VBoxContainer/Label")
+@onready var text = $MarginContainer/Panel/Label
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
