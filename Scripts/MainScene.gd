f9295297e699528f7cc20ac1f02ef736976d4f3f
MikeyMictan1 <michaeltansikorski@gmail.com>
2025-10-18 18:15:16 +0100

    Main menu buttons working

--- Scripts/MainScene.gd
+++ Scripts/MainScene.gd
@@ -10,3 +10,11 @@ func _exit_tree():
 	# Reset game state when leaving the game scene
 	GameManager.set_in_game(false)
 	GameManager.set_paused(false)
+
+
+func _on_begin_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/HelpfulDocuments.tscn")
+
+
+func _on_profile_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/profile_page.tscn")
