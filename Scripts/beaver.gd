d9c3bbfb6d6fbb67535e1ae10f3c95dd903a987d
slappinwack <crystalngshulu9909@gmail.com>
2025-10-19 00:56:41 +0100

    Minor

--- Scripts/beaver.gd
+++ Scripts/beaver.gd
@@ -5,8 +5,12 @@ extends Sprite2D
 @onready var tail: AnimatedSprite2D = $Tail
 
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		beaver.texture =  load("res://Assets/beavers/"+ cur_beaver +".png")
+		
 	beaver.texture = load("res://Assets/beavers/"+ cur_beaver +".png")
 	
 	if "point" in cur_beaver:
