49b3eed55b27275ba5b8313fc4a0d2b5807bfac0
irene-peleteiro <ipeleteiro@yahoo.com>
2025-10-19 05:48:52 +0100

    happy beaverrrrrrrrr

--- Scripts/helpful_documents.gd
+++ Scripts/helpful_documents.gd
@@ -6,6 +6,7 @@ extends Node2D
 @onready var trusted_recent_events_site = $CanvasLayer/Screen/HelpDocContainer/trusted_recent_events
 
 @onready var helpful_documents_screen = $CanvasLayer/Screen/HelpDocContainer/helpful_doc_panel
+@onready var beaver: Sprite2D = $CanvasLayer/Beaver
 
 @onready var news_template: Panel = $CanvasLayer/NewsTemplate
 @onready var buttons_enabled: Panel = $CanvasLayer/ButtonsEnabled
@@ -24,12 +25,22 @@ func _ready() -> void:
 
 	continue_button.visible = false
 	buttons_enabled.visible = false
+	beaver.visible = false
 
 
 func _on_submit_button_pressed() -> void:
-	news_template.get_results()
+	var result = news_template.get_results()
 	continue_button.visible = true
 	buttons_enabled.visible = true
+	if result == 7:
+		beaver.make_happy()
+	else:
+		beaver.make_sad()
+	if "outfit" in BrevanGlobal.current_outfit:	
+		beaver.cur_beaver = outfit_num_to_name(BrevanGlobal.current_outfit)
+	else:
+		beaver.cur_beaver = "base_beaver"
+	beaver.visible = true
 
 func _on_continue_button_pressed() -> void:
 	# if we've finished CHUNK_SIZE articles in the session, go to Results scene
@@ -39,6 +50,7 @@ func _on_continue_button_pressed() -> void:
 	news_template.hide_results()
 	continue_button.visible = false
 	buttons_enabled.visible = false
+	beaver.visible = false
 	current_article += 1
 
 	# persist progress in the autoload so sessions survive scene changes
@@ -73,3 +85,20 @@ func _on_back_button_pressed() -> void:
 
 func _on_back_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/MainScene.tscn")
+
+
+func outfit_num_to_name(outfit):
+	if outfit == "outfit1":
+		return "brainrot"
+	elif outfit == "outfit2":
+		return "chappell"
+	elif outfit == "outfit3":
+		return "cowboy"
+	elif outfit == "outfit4":
+		return "louis"
+	elif outfit == "outfit5":
+		return "mariachi"
+	elif outfit == "outfit6":
+		return "suit"
+	elif outfit == "base":
+		return "base_beaver"
