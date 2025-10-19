f94d0e298f402649d7eb1ef246f164ee7d624531
tgh9000 <144217697+tgh9000@users.noreply.github.com>
2025-10-19 07:17:01 +0100

    email stuff

---
+++ Scenes/email_check.gd
@@ -0,0 +1,80 @@
+extends Node2D
+
+@onready var trusted_author_site = $CanvasLayer/Screen/HelpDocContainer/trusted_authors
+@onready var trusted_publisher_site = $CanvasLayer/Screen/HelpDocContainer/trusted_publishers
+@onready var trusted_domain_site = $CanvasLayer/Screen/HelpDocContainer/trusted_domains
+@onready var trusted_recent_events_site = $CanvasLayer/Screen/HelpDocContainer/trusted_recent_events
+
+@onready var helpful_documents_screen = $CanvasLayer/Screen/HelpDocContainer/helpful_doc_panel
+
+@onready var news_template: Panel = $"CanvasLayer/Email Template"
+@onready var buttons_enabled: Panel = $CanvasLayer/ButtonsEnabled
+@onready var continue_button: Button = $CanvasLayer/ContinueButton
+var current_email: int
+
+
+
+#user input bit
+@onready var textInput = $"user input/Panel2/LineEdit"
+@onready var userInputTab = $"user input"
+var username
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if current_email:
+		news_template.email_id = current_email
+	else:
+		current_email = 0
+		news_template.email_id = current_email
+	continue_button.visible = false
+	buttons_enabled.visible = false
+
+
+func _UserInput () -> void:
+	username = textInput.text  # get the text the user entered
+	news_template._userinput(username)
+	
+	
+
+func _on_submit_button_pressed() -> void:
+	news_template.get_results()
+	continue_button.visible = true
+	buttons_enabled.visible = true
+
+func _on_continue_button_pressed() -> void:
+	news_template.hide_results()
+	continue_button.visible = false
+	buttons_enabled.visible = false
+	current_email += 1
+	news_template.email_id = current_email
+	news_template.load_article()
+
+func _on_trusted_author_button_pressed() -> void:
+	helpful_documents_screen.visible = false
+	trusted_author_site.visible = true
+
+func _on_trusted_publisher_button_pressed() -> void:
+	helpful_documents_screen.visible = false
+	trusted_publisher_site.visible = true
+
+func _on_trusted_domain_button_pressed() -> void:
+	helpful_documents_screen.visible = false
+	trusted_domain_site.visible = true
+
+func _on_trusted_recent_events_button_pressed() -> void:
+	helpful_documents_screen.visible = false
+	trusted_recent_events_site.visible = true
+
+func _on_back_button_pressed() -> void:
+	helpful_documents_screen.visible = true
+	trusted_author_site.visible = false
+	trusted_publisher_site.visible = false
+	trusted_domain_site.visible = false
+	trusted_recent_events_site.visible = false
+
+func _on_back_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MainScene.tscn")
+
+
+func _on_button_pressed() -> void:
+	_UserInput()
+	userInputTab.visible=false
